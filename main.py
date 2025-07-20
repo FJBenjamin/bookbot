6fe7feadfd427f56c7dbf70cdc3423ddaa436429
FJBenjamin <Fabien.J.Benjamin@gmail.com>
2025-07-20 17:26:49 -0500

    created new function get_character_count(), imported into main, called function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_count
 
 def get_book_text(book_path):
     with open(book_path) as book:
@@ -6,6 +6,9 @@ def get_book_text(book_path):
     return book_contents 
 
 def main():
-    print(str(get_num_words("books/frankenstein.txt")) + " words found in the document")
+    word_count = str(get_num_words("books/frankenstein.txt")) + " words found in the document"
+    character_count = get_character_count("books/frankenstein.txt")
 
+    print(word_count)
+    print(character_count)
 main() 
--- stats.py
+++ stats.py
@@ -2,3 +2,17 @@ def get_num_words(book_path):
     with open(book_path) as book:
         num_words = len(book.read().split())
     return num_words
+
+def get_character_count(book_path):
+    char_dict = {}
+   
+    with open(book_path) as book:
+        char_list = list(book.read().lower())
+
+        for char in char_list:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+
+    return char_dict 
