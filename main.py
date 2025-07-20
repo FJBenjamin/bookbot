c47cbcaaea902f4d3061748c33c2e5654bd68bc0
FJBenjamin <Fabien.J.Benjamin@gmail.com>
2025-07-20 16:03:24 -0500

    created function get_book_text() which takes a file path and returns the book contents as a string

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open(book_path) as book:
+        book_contents = book.read()
+    return book_contents 
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main() 
